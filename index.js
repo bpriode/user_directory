381f56a2602b81002692b98a6d8fa844a5443e54
Bethany Priode <bpriode@gmail.com>
2017-08-17 22:48:29 -0400

    individual users fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express         = require('express');
-const data           = require('./data.js');
+const data            = require('./data.js');
 const mustacheExpress = require('mustache-express');
 const app             = express();
 
@@ -14,9 +14,12 @@ app.get('/', function (req, res) {
   res.render('index', data);
 });
 
-app.get('/listing/:id', function (req, res) {
-  let users = data.users[req.params.id -1];
-  res.render('listing', data);
+app.get('/listing/:id', function (req, res) {  //puttting a colon in a route signals the dynamic part
+  // let id = req.params.id; //params only goes with request. When a client sends you information
+  // let user = data.users.find(function(user) {
+  //   return user.id == id;
+  // });
+  res.render('listing', data.users[req.params.id -1]);
 });
 
 app.listen(3000,function() {

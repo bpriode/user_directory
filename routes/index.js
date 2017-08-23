b9850db21ac9fa03a9360bfddc6d36e4496ebb7b
Bethany Priode <bpriode@gmail.com>
2017-08-22 21:18:38 -0400

    individual profiles working

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ const getListings = function(req, res, next) {
   let getData = function (db, callback) {
     let users = db.collection('users');
 
-    users.find({}).toArray().then(function(users) {
+    users.find({}).sort({'name': 1}).toArray().then(function(users) {
         data = users;
         callback();
     });
@@ -47,7 +47,7 @@ const getLooking = function(req, res, next) {
   let getData = function (db, callback) {
     let users = db.collection('users');
 
-    users.find({'job': null}).toArray().then(function(users) {
+    users.find({'job': null}).sort({'name': 1}).toArray().then(function(users) {
         data = users;
         callback();
     });
@@ -73,36 +73,22 @@ const getEmployed = function(req, res, next) {
   let getData = function (db, callback) {
     let users = db.collection('users');
 
-    users.find({'job': {$nin: [null]}}).toArray().then(function(users) {
+    users.find({'job': {$nin: [null]}}).sort({'name': 1}).toArray().then(function(users) {
         data = users;
         callback();
     });
   };
 };
 
-const getCountry = function(req, res, next) {
-  let MongoClient = require('mongodb').MongoClient;
-  let assert = require('assert');
 
-  let url = "mongodb://localhost:27017/robots";
 
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
+let getData = function (db, callback) {
+  let users = db.collection('users');
 
-    getData(db, function() {
-      db.close();
-      next();
-    });
-  });
-
-  let getData = function (db, callback) {
-    let users = db.collection('users');
-
-    users.find({}).toArray().then(function(users) {
+    users.find({}).toArray().sort({'name': 1}).then(function(users) {
         data = users;
         callback();
     });
-  };
 };
 
 
@@ -118,10 +104,6 @@ router.get('/looking', getLooking, function (req, res) {
 
 router.get('/employed', getEmployed, function (req, res) {
   res.render('employed', {users: data});
-});
-
-router.get('/country', getCountry, function (req, res) {
-  res.render('country', {users: data});
 });
 
 router.get('/listing/:id', getListings, function (req, res) {
